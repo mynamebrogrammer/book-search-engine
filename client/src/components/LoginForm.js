472fe5d1c3d4bf618b71170856f3436ba11035cb
Habib Maksoud <habibmaksoud@gmail.com>
2023-05-09 14:08:44 -0700

    first commit to push to herou

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,7 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -26,7 +25,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await LOGIN_USER(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
