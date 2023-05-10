538fdaa5e91c71d6645c25751f18c4b5f87d3c9a
Habib Maksoud <habibmaksoud@gmail.com>
2023-05-10 01:59:44 -0700

    fixed signupForm

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
@@ -16,6 +17,8 @@ const SignupForm = () => {
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  const [addUser, { error, data }] = useMutation(ADD_USER); // Use the useMutation hook to execute the mutation
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -27,13 +30,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await ADD_USER(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // Access the response data from the mutation
+      const { token, user } = data.addUser;
 
-      const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
     } catch (err) {
