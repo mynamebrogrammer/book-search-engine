1035958514d0559f94566ad1f2f637e272c6e4a0
Habib Maksoud <habibmaksoud@gmail.com>
2023-05-11 14:03:32 -0700

    fixed server issue

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -35,7 +35,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        savedBooks(bookData: BookInput!): User
+        saveBook(bookData: BookInput!): User
         removeBook(bookId: String): User
     }
 `;
